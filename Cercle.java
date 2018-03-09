21dd1f5fe6f19a2b3de01a98aaa12cecff1b2cd0
Domantas <domantassidorenkovas@gmail.com>
2018-03-09 17:08:01 +0100

    cercle funcions

--- Cercle.java
+++ Cercle.java
@@ -13,6 +13,22 @@ public class Cercle{
      y=yy;
 
   }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
  public Cercle(){
      
  }
